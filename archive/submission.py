8be2758513050c696576043ab666f0e32d2c5b4e
saadhzubairi <shz2020@gmail.com>
2025-08-15 00:08:40 -0400

    Added Project 3 code and report on public repo

--- archive/submission.py
+++
@@ -1,6 +0,0 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-def submit_to_kaggle(filename, message):
-    api = KaggleApi()
-    api.authenticate()
-    api.competition_submit(filename, f"{message}", 'deep-learning-spring-2025-project-1')
